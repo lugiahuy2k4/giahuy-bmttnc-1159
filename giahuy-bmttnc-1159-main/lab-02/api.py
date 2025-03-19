05854129c7ee6817c98dc3eb9a42cbce3340d9ef
lugiahuy2k4 <lugiahuy1812@gmail.com>
2025-03-19 11:12:27 +0700

    Add files via upload

--- giahuy-bmttnc-1159-main/lab-02/api.py
+++ giahuy-bmttnc-1159-main/lab-02/api.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
-
+from cipher.railfence import RailFenceCipher
+from cipher.transposition import TranspositionCipher
 
 app = Flask(__name__)
 
 caesar_cipher = CaesarCipher()
+
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
@@ -13,6 +15,7 @@ def caesar_encrypt():
     key = int(data['key'])
     encrypt_text = caesar_cipher.encrypt_text(plain_text, key)
     return jsonify({'encrypted_message': encrypt_text})
+
 @app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
     data = request.json
@@ -22,6 +25,7 @@ def caesar_decrypt():
     return jsonify({'decrypted_message': decrypt_text})
 
 vigenere_cipher = VigenereCipher()
+
 @app.route('/api/vigenere/encrypt', methods=['POST'])
 def vigenere_encrypt():
     data = request.json
@@ -35,9 +39,44 @@ def vigenere_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
     key = data['key']
-    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text,key)
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
+
+railfence_cipher = RailFenceCipher()
+@app.route('/api/railfence/encrypt', methods=['POST'])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/railfence/decrypt', methods=['POST'])
+def decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+transposition_cipher = TranspositionCipher()
+
+@app.route('/api/transposition/encrypt', methods=['POST'])
+def transposition_encrypt():
+    data = request.get_json()
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = transposition_cipher.encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/transposition/decrypt', methods=['POST'])
+def transposition_decrypt():
+    data = request.get_json()
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = transposition_cipher.decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
